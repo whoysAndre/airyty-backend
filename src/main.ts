803085b08aba2243a9de84ddee3668fe1ef3f464
whoysAndre <yellowcata2121@gmail.com>
2026-04-01 18:25:24 -0500

    FEATURES: Add domain CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://airyty-frontend.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
